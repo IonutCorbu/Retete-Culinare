cd5d4d3dedc30f44f154a9cdd0e852b6809f28e2
corbu.ionut <85988309+IonutCorbu@users.noreply.github.com>
2022-11-29 00:43:09 +0200

    Sursa C#
    Codul corespunzător paginii Contul Meu

---
+++ ContulMeu.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proiect
+{
+    /// <summary>
+    /// Interaction logic for ContulMeu.xaml
+    /// </summary>
+    public partial class ContulMeu : Page
+    {
+        string User;
+        public ContulMeu(string user)
+        {
+            InitializeComponent();
+            User = user;
+            Persoana.Content = user;
+            copyright.Foreground = Brushes.Gray;
+            copyright.Opacity = 0.7;
+        }
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            //Main.Navigate(new Login(Persoana.Content.toString()));
+        }
+        private void Arata_retete(object sender, RoutedEventArgs e)
+        {
+            Main.Navigate(new ArataRetete("IonutCorbu"));
+        }
+        private void Adaugaretete(object sender, RoutedEventArgs e)
+        {
+            Main.Navigate(new AdaugaReteta("IonutCorbu"));
+        }
+    }
+}
